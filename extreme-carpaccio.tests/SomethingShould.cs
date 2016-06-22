77aaeaed15142372c31c3ffa8910cf01f6a37a9d
jordan38 <jordan.content@outlook.fr>
2016-06-22 08:53:11 +0200

    Mise en place du projet de test

---
+++ extreme-carpaccio.tests/SomethingShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace extreme_carpaccio.tests
+{
+    public class SomethingShould
+    {
+        [Test]
+        public void DoSomething()
+        {
+            Check.That("").IsNull();
+        }
+    }
+}
